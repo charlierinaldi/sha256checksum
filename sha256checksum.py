cac3bc3ada5cbe612152cd5d79f624676f8ff890
Chaz <charlierinaldi09@gmail.com>
2023-04-22 03:00:26 -0400

    Added tkinter file explorer gui

--- sha256checksum.py
+++ sha256checksum.py
@@ -2,23 +2,28 @@
 #                                                                                                                    #
 #                                             Don't get pwn'd, kids                                                  #
 
-
+from tkinter import filedialog
 import subprocess
+import os
 
-filepath = input("Paste the filepath for the file to be checked: ")
+# Stack overflow said you should do this to prevent 
+
+file = filedialog.askopenfile()
+if file:
+    filepath = os.path.abspath(file.name)
 
 s = subprocess.run(['certutil', '-hashfile', f'{filepath}',  'SHA256'], stdout=subprocess.PIPE)
 pipeReturn = s.stdout.decode('utf-8')
 
 print("\n" + pipeReturn)
 
-requestedsha = input("Now paste the hash as indicated on the official website: ")
+requestedsha = input("Now paste the hash as indicated on the official website (please): ")
 
 # Random lesson learned: if 2 strings don't appear to be comparing correctly, use print(acsii(strName)) to see escaped chars n shit
 
 newrequestedsha = "SHA256 hash of " + filepath + ":\r\n" + requestedsha + "\r\n" + "CertUtil: -hashfile command completed successfully.\r\n" # Too lazy to format the 3 lines returned from stdout, so just mimicking the other 2 lines here o7
 
 if pipeReturn == newrequestedsha:
-    print("\n\n************ 8======D ************\n\nMatches! Always wrap your willy :)\n\n************ C======D ************\n")
+    print("\n\n************ 8======D ************\n\nMatches! Always wrap your willy :-)\n\n************ C======D ************\n")
 else:
     print("\n\n********************\n\nDelete this shit, you almost just got space herpies...\n\n********************\n")
